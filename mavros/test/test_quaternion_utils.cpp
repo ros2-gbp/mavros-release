39ff473bcb003c57670db7af465c770fd63ea67a
Vladimir Ermakov <vooon341@gmail.com>
2023-09-09 14:44:38 +0000

    Rebase from 'upstream'

--- mavros/test/test_quaternion_utils.cpp
+++ mavros/test/test_quaternion_utils.cpp
@@ -12,12 +12,11 @@
  */
 
 #include <gtest/gtest.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
 
 #include <rclcpp/rclcpp.hpp>
 #include <mavros/frame_tf.hpp>
-
-#include <tf2/LinearMath/Quaternion.h>
-#include <tf2/LinearMath/Matrix3x3.h>
 
 using namespace mavros;     // NOLINT
 
