986af154e34006d694d9e83fe016a859de31db20
Vladimir Ermakov <vooon341@gmail.com>
2026-08-22 11:13:37 +0000

    Revert "Trimmed the branch to only the mavros_examples sub directory"
    This reverts commit 5aa04742d7952aecbf1f3b16955f1cfe007c24b1.

---
+++ libmavconn/include/mavconn/thread_utils.hpp
@@ -0,0 +1,102 @@
+//
+// libmavconn
+// Copyright 2014,2015,2016,2021 Vladimir Ermakov, All rights reserved.
+//
+// This file is part of the mavros package and subject to the license terms
+// in the top-level LICENSE file of the mavros repository.
+// https://github.com/mavlink/mavros/tree/master/LICENSE.md
+//
+/**
+ * @brief some useful utils
+ * @file thread_utils.h
+ * @author Vladimir Ermakov <vooon341@gmail.com>
+ *
+ * @addtogroup mavutils
+ * @{
+ *  @brief Some useful utils
+ */
+
+#pragma once
+#ifndef MAVCONN__THREAD_UTILS_HPP_
+#define MAVCONN__THREAD_UTILS_HPP_
+
+#include <pthread.h>
+
+#include <array>
+#include <cstdio>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <utility>
+
+namespace mavconn
+{
+namespace utils
+{
+
+/**
+ * @brief Make printf-formatted std::string
+ *
+ * Fast path formats into a small stack buffer with a single snprintf; only when
+ * the output would not fit does it compute the exact size and re-format into a
+ * heap string. Returns an empty string on an encoding error.
+ */
+template<typename ... Args>
+std::string format(const std::string & fmt, Args... args)
+{
+  std::array<char, 256> stack{};
+  const int written = std::snprintf(stack.data(), stack.size(), fmt.c_str(), args ...);
+  if (written < 0) {
+    return {};   // encoding error in the format or arguments
+  }
+  if (static_cast<size_t>(written) < stack.size()) {
+    return std::string(stack.data(), written);
+  }
+
+  std::string ret;
+  ret.resize(static_cast<size_t>(written));
+  std::snprintf(ret.data(), ret.size() + 1, fmt.c_str(), args ...);
+  return ret;
+}
+
+/**
+ * @brief Set name to current thread, printf-like
+ * @param[in] name name for thread
+ * @return true if success
+ *
+ * @note Only for Linux target
+ * @todo add for other posix system
+ */
+template<typename ... Args>
+bool set_this_thread_name(const std::string & name, Args && ... args)
+{
+  auto new_name = format(name, std::forward<Args>(args)...);
+
+#ifdef __APPLE__
+  return pthread_setname_np(new_name.c_str()) == 0;
+#else
+  pthread_t pth = pthread_self();
+  return pthread_setname_np(pth, new_name.c_str()) == 0;
+#endif
+}
+
+/**
+ * @brief Convert to string objects with operator <<
+ */
+template<typename T>
+inline const std::string to_string_ss(T & obj)
+{
+  std::ostringstream ss;
+  ss << obj;
+  return ss.str();
+}
+
+constexpr size_t operator"" _KiB(unsigned long long sz)      // NOLINT
+{
+  return sz * 1024;
+}
+
+}  // namespace utils
+}  // namespace mavconn
+
+#endif  // MAVCONN__THREAD_UTILS_HPP_
---
+++ libmavconn/include/mavconn/udp.hpp
@@ -0,0 +1,113 @@
+//
+// libmavconn
+// Copyright 2013,2014,2015,2016,2021 Vladimir Ermakov, All rights reserved.
+//
+// This file is part of the mavros package and subject to the license terms
+// in the top-level LICENSE file of the mavros repository.
+// https://github.com/mavlink/mavros/tree/master/LICENSE.md
+//
+/**
+ * @brief MAVConn UDP link class
+ * @file mavconn_udp.h
+ * @author Vladimir Ermakov <vooon341@gmail.com>
+ *
+ * @addtogroup mavconn
+ * @{
+ */
+
+#pragma once
+#ifndef MAVCONN__UDP_HPP_
+#define MAVCONN__UDP_HPP_
+
+#include <atomic>
+#include <deque>
+#include <memory>
+#include <string>
+
+#include <asio.hpp>
+#include <mavconn/interface.hpp>
+#include <mavconn/io_context_runner.hpp>
+#include <mavconn/msgbuffer.hpp>
+
+namespace mavconn
+{
+
+/**
+ * @brief UDP interface
+ *
+ * @note IPv4 only
+ */
+class MAVConnUDP : public MAVConnInterface,
+  public std::enable_shared_from_this<MAVConnUDP>
+{
+public:
+  static constexpr auto DEFAULT_BIND_HOST = "localhost";
+  static constexpr auto DEFAULT_BIND_PORT = 14555;
+  static constexpr auto DEFAULT_REMOTE_HOST = "";
+  static constexpr auto DEFAULT_REMOTE_PORT = 14550;
+  //! Markers for broadcast modes. Not valid domain names.
+  static constexpr auto BROADCAST_REMOTE_HOST = "***i want broadcast***";
+  static constexpr auto PERMANENT_BROADCAST_REMOTE_HOST = "***permanent broadcast***";
+
+  /**
+   * @param[id] bind_host    bind host
+   * @param[id] bind_port    bind port
+   * @param[id] remote_host  remote host (optional)
+   * @param[id] remote_port  remote port (optional)
+   * @param[id] shared_io    optional external io_context. If provided, caller owns
+   *                         its execution/threading lifecycle.
+   */
+  MAVConnUDP(
+    uint8_t system_id = 1, uint8_t component_id = MAV_COMP_ID_UDP_BRIDGE,
+    std::string bind_host = DEFAULT_BIND_HOST, uint16_t bind_port = DEFAULT_BIND_PORT,
+    std::string remote_host = DEFAULT_REMOTE_HOST,
+    uint16_t remote_port = DEFAULT_REMOTE_PORT,
+    asio::io_context * shared_io = nullptr);
+
+  virtual ~MAVConnUDP();
+
+  void connect(
+    const ReceivedCb & cb_handle_message,
+    const ClosedCb & cb_handle_closed_port = ClosedCb()) override;
+  void close() override;
+
+  void send_message(const mavlink::mavlink_message_t * message) override;
+  void send_message(const mavlink::Message & message, const uint8_t source_compid) override;
+  void send_bytes(const uint8_t * bytes, size_t length) override;
+
+  [[nodiscard]] inline bool is_open() override
+  {
+    return socket.is_open();
+  }
+
+  [[nodiscard]] std::string get_remote_endpoint() const;
+
+private:
+  IoContextRunner io_runner;
+  asio::io_context & io_context;
+  bool permanent_broadcast;
+
+  std::atomic<bool> remote_exists;
+  asio::ip::udp::socket socket;
+  asio::ip::udp::endpoint remote_ep;
+  asio::ip::udp::endpoint recv_ep;
+  asio::ip::udp::endpoint last_remote_ep;
+  asio::ip::udp::endpoint bind_ep;
+
+  std::atomic<bool> tx_in_progress;
+  std::deque<MsgBuffer> tx_q;
+  std::array<uint8_t, MsgBuffer::MAX_SIZE> rx_buf;
+  std::mutex mutex;
+
+  void do_recvfrom();
+  void do_sendto(bool check_tx_state);
+
+  /**
+   * Stop io_context.
+   */
+  void stop();
+};
+
+}  // namespace mavconn
+
+#endif  // MAVCONN__UDP_HPP_
