c36e80de9ab063322e3bca498426465e1b075f79
Vladimir Ermakov <vooon341@gmail.com>
2026-08-22 11:26:37 +0000

    Revert "Trimmed the branch to only the mavros_msgs sub directory"
    This reverts commit dd5ab3012eda7c67f420da898bbaecb3094ca7b1.

---
+++ libmavconn/include/mavconn/io_context_runner.hpp
@@ -0,0 +1,127 @@
+//
+// libmavconn
+// Copyright 2026 Vladimir Ermakov, contributors.
+//
+// This file is part of the mavros package and subject to the license terms
+// in the top-level LICENSE file of the mavros repository.
+// https://github.com/mavlink/mavros/tree/master/LICENSE.md
+//
+
+#pragma once
+#ifndef MAVCONN__IO_CONTEXT_RUNNER_HPP_
+#define MAVCONN__IO_CONTEXT_RUNNER_HPP_
+
+#include <atomic>
+#include <memory>
+#include <thread>
+#include <utility>
+
+#include <asio.hpp>
+
+namespace mavconn
+{
+
+/**
+ * @brief Small utility to unify owned/shared io_context lifecycle handling.
+ */
+class IoContextRunner
+{
+public:
+  explicit IoContextRunner(asio::io_context * shared_io = nullptr)
+  : io_owner_(shared_io ? nullptr : std::make_shared<asio::io_context>()),
+    io_(shared_io ? *shared_io : *io_owner_),
+    io_work_(shared_io ? nullptr :
+      std::make_unique<asio::executor_work_guard<asio::io_context::executor_type>>(
+      asio::make_work_guard(io_))),
+    owns_thread_(shared_io == nullptr),
+    is_running_(false)
+  {}
+
+  [[nodiscard]] asio::io_context & io()
+  {
+    return io_;
+  }
+
+  [[nodiscard]] bool owns_thread() const
+  {
+    return owns_thread_;
+  }
+
+  [[nodiscard]] bool is_running() const
+  {
+    return is_running_.load();
+  }
+
+  template<typename Fn>
+  void start(Fn && fn)
+  {
+    if (!owns_thread_) {
+      return;
+    }
+
+    io_thread_ = std::jthread(
+      [this, f = std::forward<Fn>(fn)]() mutable {
+        is_running_ = true;
+        f();
+        is_running_ = false;
+      });
+  }
+
+  void stop_owned()
+  {
+    if (!owns_thread_) {
+      return;
+    }
+
+    io_thread_.request_stop();
+    io_work_.reset();
+    io_.stop();
+  }
+
+  void join_owned()
+  {
+    if (!owns_thread_) {
+      return;
+    }
+
+    if (!io_thread_.joinable()) {
+      return;
+    }
+
+    if (std::this_thread::get_id() == io_thread_.get_id()) {
+      // Cannot join from the same thread; detach so destructor can't terminate.
+      io_thread_.detach();
+      return;
+    }
+
+    io_thread_.join();
+  }
+
+  void reset_owned()
+  {
+    if (!owns_thread_) {
+      return;
+    }
+
+    io_.restart();
+  }
+
+  void shutdown_owned()
+  {
+    stop_owned();
+    join_owned();
+    reset_owned();
+  }
+
+private:
+  std::shared_ptr<asio::io_context> io_owner_;
+  asio::io_context & io_;
+  std::unique_ptr<asio::executor_work_guard<asio::io_context::executor_type>> io_work_;
+  bool owns_thread_;
+  std::atomic<bool> is_running_;
+  std::jthread io_thread_;
+};
+
+}  // namespace mavconn
+
+#endif  // MAVCONN__IO_CONTEXT_RUNNER_HPP_
